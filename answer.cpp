3abe05b276077db606b123a56f4936836da92f35
lgeu <nagiss000@gmail.com>
2022-02-15 19:27:24 +0900

    start C++ coding

--- answer.cpp
+++ answer.cpp
@@ -21,6 +21,22 @@
 #warning "NDEBUG マクロが定義されてないよ！動作が遅くなるかもしれないよ！"
 #endif
 
+#ifdef __GNUC__
+#ifdef __clang__
+#pragma clang attribute push(__attribute__((target("arch=skylake"))), apply_to = function)
+/* 最後に↓を貼る
+#ifdef __clang__
+#pragma clang attribute pop
+#endif
+*/
+#else // defined(__clang__)
+//#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,fma,avx,avx2,tune=native")
+//#pragma GCC optimize("unroll-loops")
+#endif // defined(__clang__)
+#endif // defined(__GNUC__)
+
 using namespace std;
 
 namespace nn {
@@ -1148,6 +1164,486 @@ template <int dim> struct alignas(32) ResBlock {
 
 } // namespace nn
 
+#define rep(i, n) for (auto i = 0; (i) < (n); (i)++)
+#define rep1(i, n) for (auto i = 1; (i) <= (n); (i)++)
+#define rep3(i, s, n) for (auto i = (s); (i) < (n); (i)++)
+
+//#define NDEBUG
+
+#ifndef NDEBUG
+#define VISUALIZE
+#endif
+
+#ifndef NDEBUG
+#define ASSERT(expr, ...)                                                                                                                            \
+    do {                                                                                                                                             \
+        if (!(expr)) {                                                                                                                               \
+            printf("%s(%d): Assertion failed.\n", __FILE__, __LINE__);                                                                               \
+            printf(__VA_ARGS__);                                                                                                                     \
+            abort();                                                                                                                                 \
+        }                                                                                                                                            \
+    } while (false)
+#else
+#define ASSERT(...)
+#endif
+
+#define ASSERT_RANGE(value, left, right) ASSERT((left <= value) && (value < right), "`%s` (%d) is out of range [%d, %d)", #value, value, left, right)
+
+#define CHECK(var)                                                                                                                                   \
+    do {                                                                                                                                             \
+        cerr << #var << '=' << var << endl;                                                                                                          \
+    } while (false)
+
+// ========================== utils ==========================
+
+using namespace std;
+using ll = long long;
+constexpr double PI = 3.1415926535897932;
+
+template <class T, class S> inline bool chmin(T& m, S q) {
+    if (m > q) {
+        m = q;
+        return true;
+    } else
+        return false;
+}
+
+template <class T, class S> inline bool chmax(T& m, const S q) {
+    if (m < q) {
+        m = q;
+        return true;
+    } else
+        return false;
+}
+
+// 乱数
+struct Random {
+    using ull = unsigned long long;
+    unsigned seed;
+    inline Random(const unsigned& seed_) : seed(seed_) { ASSERT(seed != 0u, "Seed should not be 0."); }
+    const inline unsigned& next() {
+        seed ^= seed << 13;
+        seed ^= seed >> 17;
+        seed ^= seed << 5;
+        return seed;
+    }
+    // (0.0, 1.0)
+    inline double random() { return (double)next() * (1.0 / (double)0x100000000ull); }
+    // [0, right)
+    inline int randint(const int& right) { return (ull)next() * right >> 32; }
+    // [left, right)
+    inline int randint(const int& left, const int& right) { return ((ull)next() * (right - left) >> 32) + left; }
+};
+
+// 2 次元ベクトル
+template <typename T> struct Vec2 {
+    /*
+    y 軸正は下方向
+    x 軸正は右方向
+    回転は時計回りが正（y 軸正を上と考えると反時計回りになる）
+    */
+    T y, x;
+    constexpr inline Vec2() = default;
+    constexpr inline Vec2(const T& arg_y, const T& arg_x) : y(arg_y), x(arg_x) {}
+    inline Vec2(const Vec2&) = default;            // コピー
+    inline Vec2(Vec2&&) = default;                 // ムーブ
+    inline Vec2& operator=(const Vec2&) = default; // 代入
+    inline Vec2& operator=(Vec2&&) = default;      // ムーブ代入
+    template <typename S> constexpr inline Vec2(const Vec2<S>& v) : y((T)v.y), x((T)v.x) {}
+    inline Vec2 operator+(const Vec2& rhs) const { return Vec2(y + rhs.y, x + rhs.x); }
+    inline Vec2 operator+(const T& rhs) const { return Vec2(y + rhs, x + rhs); }
+    inline Vec2 operator-(const Vec2& rhs) const { return Vec2(y - rhs.y, x - rhs.x); }
+    template <typename S> inline Vec2 operator*(const S& rhs) const { return Vec2(y * rhs, x * rhs); }
+    inline Vec2 operator*(const Vec2& rhs) const { // x + yj とみなす
+        return Vec2(x * rhs.y + y * rhs.x, x * rhs.x - y * rhs.y);
+    }
+    template <typename S> inline Vec2 operator/(const S& rhs) const {
+        ASSERT(rhs != 0.0, "Zero division!");
+        return Vec2(y / rhs, x / rhs);
+    }
+    inline Vec2 operator/(const Vec2& rhs) const { // x + yj とみなす
+        return (*this) * rhs.inv();
+    }
+    inline Vec2& operator+=(const Vec2& rhs) {
+        y += rhs.y;
+        x += rhs.x;
+        return *this;
+    }
+    inline Vec2& operator-=(const Vec2& rhs) {
+        y -= rhs.y;
+        x -= rhs.x;
+        return *this;
+    }
+    template <typename S> inline Vec2& operator*=(const S& rhs) const {
+        y *= rhs;
+        x *= rhs;
+        return *this;
+    }
+    inline Vec2& operator*=(const Vec2& rhs) {
+        *this = (*this) * rhs;
+        return *this;
+    }
+    inline Vec2& operator/=(const Vec2& rhs) {
+        *this = (*this) / rhs;
+        return *this;
+    }
+    inline bool operator!=(const Vec2& rhs) const { return x != rhs.x || y != rhs.y; }
+    inline bool operator==(const Vec2& rhs) const { return x == rhs.x && y == rhs.y; }
+    inline void rotate(const double& rad) { *this = rotated(rad); }
+    inline Vec2<double> rotated(const double& rad) const { return (*this) * rotation(rad); }
+    static inline Vec2<double> rotation(const double& rad) { return Vec2(sin(rad), cos(rad)); }
+    static inline Vec2<double> rotation_deg(const double& deg) { return rotation(PI * deg / 180.0); }
+    inline Vec2<double> rounded() const { return Vec2<double>(round(y), round(x)); }
+    inline Vec2<double> inv() const { // x + yj とみなす
+        const double norm_sq = l2_norm_square();
+        ASSERT(norm_sq != 0.0, "Zero division!");
+        return Vec2(-y / norm_sq, x / norm_sq);
+    }
+    inline double l2_norm() const { return sqrt(x * x + y * y); }
+    inline double l2_norm_square() const { return x * x + y * y; }
+    inline T l1_norm() const { return std::abs(x) + std::abs(y); }
+    inline double abs() const { return l2_norm(); }
+    inline double phase() const { // [-PI, PI) のはず
+        return atan2(y, x);
+    }
+    inline double phase_deg() const { // [-180, 180) のはず
+        return phase() / PI * 180.0;
+    }
+};
+template <typename T, typename S> inline Vec2<T> operator*(const S& lhs, const Vec2<T>& rhs) { return rhs * lhs; }
+template <typename T> ostream& operator<<(ostream& os, const Vec2<T>& vec) {
+    os << vec.y << ' ' << vec.x;
+    return os;
+}
+
+// 2 次元配列
+template <class T, int height, int width> struct Board {
+    array<T, height * width> data;
+    template <class Int> constexpr inline auto& operator[](const Vec2<Int>& p) { return data[width * p.y + p.x]; }
+    template <class Int> constexpr inline const auto& operator[](const Vec2<Int>& p) const { return data[width * p.y + p.x]; }
+    template <class Int> constexpr inline auto& operator[](const initializer_list<Int>& p) { return data[width * *p.begin() + *(p.begin() + 1)]; }
+    template <class Int> constexpr inline const auto& operator[](const initializer_list<Int>& p) const {
+        return data[width * *p.begin() + *(p.begin() + 1)];
+    }
+    constexpr inline void Fill(const T& fill_value) { fill(data.begin(), data.end(), fill_value); }
+    void Print() const {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                cout << data[width * y + x] << " \n"[x == width - 1];
+            }
+        }
+    }
+};
+
+// スタック  // コンストラクタ呼ぶタイミングとかが考えられてなくて良くない
+template <class T, int max_size> struct Stack {
+    array<T, max_size> data;
+    int right;
+    inline Stack() : data(), right(0) {}
+    inline Stack(const int n) : data(), right(0) { resize(n); }
+    inline Stack(const int n, const T& val) : data(), right(0) { resize(n, val); }
+    inline Stack(const initializer_list<T>& init) : data(), right(init.size()) {
+        memcpy(&data[0], init.begin(), sizeof(T) * init.size());
+    }                                                           // これ大丈夫か？
+    inline Stack(const Stack& rhs) : data(), right(rhs.right) { // コピー
+        for (int i = 0; i < right; i++) {
+            data[i] = rhs.data[i];
+        }
+    }
+    template <class S> inline Stack(const Stack<S, max_size>& rhs) : data(), right(rhs.right) {
+        for (int i = 0; i < right; i++) {
+            data[i] = rhs.data[i];
+        }
+    }
+    template <class Container> Stack& operator=(const Container& rhs) {
+        right = rhs.size();
+        ASSERT(right <= max_size, "Too big container.");
+        for (int i = 0; i < right; i++) {
+            data[i] = rhs.data[i];
+        }
+        return *this;
+    }
+    Stack& operator=(Stack&&) = default;
+    inline bool empty() const { return 0 == right; }
+    inline void push(const T& value) {
+        ASSERT_RANGE(right, 0, max_size);
+        data[right] = value;
+        right++;
+    }
+    inline T pop() {
+        right--;
+        ASSERT_RANGE(right, 0, max_size);
+        return data[right];
+    }
+    const inline T& top() const { return data[right - 1]; }
+    template <class... Args> inline void emplace(const Args&... args) {
+        ASSERT_RANGE(right, 0, max_size);
+        data[right] = T(args...);
+        right++;
+    }
+    inline void clear() { right = 0; }
+    inline void insert(const int& idx, const T& value) {
+        ASSERT_RANGE(idx, 0, right + 1);
+        ASSERT_RANGE(right, 0, max_size);
+        int i = right;
+        right++;
+        while (i != idx) {
+            data[i] = data[i - 1];
+            i--;
+        }
+        data[idx] = value;
+    }
+    inline void del(const int& idx) {
+        ASSERT_RANGE(idx, 0, right);
+        right--;
+        for (int i = idx; i < right; i++) {
+            data[i] = data[i + 1];
+        }
+    }
+    inline int index(const T& value) const {
+        for (int i = 0; i < right; i++) {
+            if (value == data[i])
+                return i;
+        }
+        return -1;
+    }
+    inline void remove(const T& value) {
+        int idx = index(value);
+        ASSERT(idx != -1, "not contain the value.");
+        del(idx);
+    }
+    inline void resize(const int& sz) {
+        ASSERT_RANGE(sz, 0, max_size + 1);
+        for (; right < sz; right++) {
+            data[right].~T();
+            new (&data[right]) T();
+        }
+        right = sz;
+    }
+    inline void resize(const int& sz, const T& fill_value) {
+        ASSERT_RANGE(sz, 0, max_size + 1);
+        for (; right < sz; right++) {
+            data[right].~T();
+            new (&data[right]) T(fill_value);
+        }
+        right = sz;
+    }
+    inline int size() const { return right; }
+    inline T& operator[](const int n) {
+        ASSERT_RANGE(n, 0, right);
+        return data[n];
+    }
+    inline const T& operator[](const int n) const {
+        ASSERT_RANGE(n, 0, right);
+        return data[n];
+    }
+    inline T* begin() { return (T*)data.data(); }
+    inline const T* begin() const { return (const T*)data.data(); }
+    inline T* end() { return (T*)data.data() + right; }
+    inline const T* end() const { return (const T*)data.data() + right; }
+    inline T& front() {
+        ASSERT(right > 0, "no data.");
+        return data[0];
+    }
+    const inline T& front() const {
+        ASSERT(right > 0, "no data.");
+        return data[0];
+    }
+    inline T& back() {
+        ASSERT(right > 0, "no data.");
+        return data[right - 1];
+    }
+    const inline T& back() const {
+        ASSERT(right > 0, "no data.");
+        return data[right - 1];
+    }
+    inline bool contains(const T& value) const {
+        for (const auto& dat : *this) {
+            if (value == dat)
+                return true;
+        }
+        return false;
+    }
+    inline vector<T> ToVector() { return vector<T>(begin(), end()); }
+    inline void Print(ostream& os = cout) {
+        for (int i = 0; i < right; i++) {
+            os << data[i] << (i == right - 1 ? "" : " ");
+        }
+        os << endl;
+    }
+};
+
+template <class T, int size = 0x100000, class KeyType = unsigned> struct MinimumHashMap {
+    // ハッシュの値が size 以下
+    array<T, size> data;
+    Stack<int, size> used;
+    constexpr static KeyType mask = size - 1;
+    inline MinimumHashMap() {
+        static_assert((size & size - 1) == 0, "not pow of 2");
+        memset(&data[0], (unsigned char)-1, sizeof(data));
+    }
+    inline T& operator[](const KeyType& key) {
+        if (data[key] == (T)-1)
+            used.push(key);
+        return data[key];
+    }
+    inline void clear() {
+        for (const auto& key : used)
+            data[key] = (T)-1;
+        used.right = 0;
+    }
+};
+
+// 時間 (秒)
+inline double time() {
+    return static_cast<double>(chrono::duration_cast<chrono::nanoseconds>(chrono::steady_clock::now().time_since_epoch()).count()) * 1e-9;
+}
+
+template <typename T> struct Slice {
+    T *left, *right;
+    inline Slice(T* const& l, T* const& r) : left(l), right(r) {}
+    inline T* begin() { return left; }
+    inline const T* begin() const { return (const T*)left; }
+    inline T* end() { return right; }
+    inline const T* end() const { return (const T*)right; }
+    inline int size() const { return distance(left, right); }
+    inline T& operator[](const int& idx) { return left[idx]; }
+    inline const T& operator[](const int& idx) const { return left[idx]; }
+};
+
+// ===========================================================================================================
+
+enum struct Directions { U, D, L, R };
+constexpr auto DIRECTION_VECS = array<Vec2<int>, 4>{Vec2<int>{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+enum struct PetType { NONE, COW, PIG, RABBIT, DOG, CAT };
+using i8 = signed char;
+
+namespace common {
+int N; // ペットの数
+int M; // 人間の数
+auto pet_types = array<PetType, 20>();
+auto pet_positions = array<Vec2<int>, 20>();
+auto human_positions = array<Vec2<int>, 10>();
+
+auto pet_history = array<Directions, 300>();
+auto current_turn = 0;
+// auto global_features = nn::Tensor < fl
+
+auto human_moves = array<char, 10>();
+auto fence_board = Board<bool, 32, 32>();
+auto human_count_board = Board<i8, 32, 32>();
+auto pet_count_board = Board<i8, 32, 32>();
+
+} // namespace common
+
+void Initialize() {
+    using namespace common;
+    cin >> N;
+    rep(i, N) {
+        int pt;
+        cin >> pet_positions[i].y >> pet_positions[i].x >> pt;
+        pet_types[i] = (PetType)pt;
+    }
+    cin >> M;
+    rep(i, M) { cin >> human_positions[i].y >> human_positions[i].x; }
+    rep(i, 32) {
+        fence_board[{i, 0}] = true;
+        fence_board[{i, 31}] = true;
+        fence_board[{0, i}] = true;
+        fence_board[{31, i}] = true;
+    }
+}
+
+void UpdateHuman() {
+    //
+    using common::fence_board;
+    using common::human_count_board;
+    using common::human_moves;
+    using common::human_positions;
+    using common::pet_count_board;
+
+    // 仕切り設置処理
+    auto set_fence = [&](const Vec2<int>& p) {
+        if (human_count_board[p] != 0 || pet_count_board[p] != 0) {
+            return false;
+        } else {
+            for (const auto& d : DIRECTION_VECS) {
+                auto neighbor = p + d;
+                if (pet_count_board[p] != 0) {
+                    return false;
+                }
+            }
+        }
+        fence_board[p] = true;
+        return true;
+    };
+
+    // 先に通行不能化処理をやる
+    rep(i, common::M) {
+        const auto& v = human_positions[i];
+        Directions d;
+        switch (human_moves[i]) {
+        // 通行不能にする
+        case 'u':
+            d = Directions::U;
+            break;
+        case 'd':
+            d = Directions::D;
+            break;
+        case 'l':
+            d = Directions::L;
+            break;
+        case 'r':
+            d = Directions::R;
+            break;
+        }
+        auto succeeded = set_fence(v + DIRECTION_VECS[(int)d]);
+        assert(succeeded);
+    }
+
+    // 移動処理をする
+    rep(i, common::M) {
+        const auto& v = human_positions[i];
+        Directions d;
+        switch (human_moves[i]) {
+        // 移動
+        case 'U':
+            d = Directions::U;
+            break;
+        case 'D':
+            d = Directions::D;
+            break;
+        case 'L':
+            d = Directions::L;
+            break;
+        case 'R':
+            d = Directions::R;
+            break;
+        }
+        Vec2<int> u = v + DIRECTION_VECS[(int)d];
+        assert(!fence_board[u]);
+        human_positions[i] = u;
+        human_count_board[u]++;
+        human_count_board[v]--;
+    }
+}
+
+void UpdatePets() {
+    //
+}
+
+void ExtractFeatures() {
+    //
+}
+
+void Predict() {
+    //
+}
+
 int main() {
     //
 }
+
+#ifdef __clang__
+#pragma clang attribute pop
+#endif
